9e89b8051af1b6fe1e07989c1e7a9ee3bd605968
Drew Haven <drew.haven@gmail.com>
2011-06-12 21:38:59 -0700

    updated main for new handler signature.

--- WoWSim.hs
+++ WoWSim.hs
@@ -16,6 +16,6 @@ main = do
     let pEntity = makeEntity "Player"
         tEntity = makeEntity "Target"
     let world = World pEntity tEntity
-        (t, log, world') = {-# SCC "sim" #-} simulate world [warrior] EvSimStart (read dur)
+        (t, log, world') = {-# SCC "sim" #-} simulate world [("Warrior", warrior)] EvSimStart (read dur)
     print $ (t, eHealth . target $ world')
 
