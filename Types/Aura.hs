6ff14bb7ea719764d0b6612f100743b41747bffe
Drew Haven <drew.haven@gmail.com>
2011-06-22 23:36:24 -0700

    finished aura functions.

--- Types/Aura.hs
+++ Types/Aura.hs
@@ -11,10 +11,10 @@ addAuraToMap :: Aura -> AuraMap -> AuraMap
 addAuraToMap aura = insert (getL auraId aura) aura
 
 removeAuraFromMap :: AuraId -> AuraMap -> AuraMap
-removeAuraFromMap = undefined
+removeAuraFromMap = delete
 
 isAuraInMap :: AuraId -> AuraMap -> Bool
-isAuraInMap = undefined
+isAuraInMap = member
 
 instance Show Aura where
     show (Aura id school typ cat f) = printf "Aura { id = %s, school = %s, type = %s, category = %s}" (show id) (show school) (show typ) (show cat)
